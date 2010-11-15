c801860216edc7ffc69f13cae0f55991f5354e56
Thanar <Thankor@gmail.com>
2010-11-15 22:28:49 +0100

    Signed-off-by: Thanar <Thankor@gmail.com>

--- src/Gaficos/Ventana_Mapa.java
+++ src/Gaficos/Ventana_Mapa.java
@@ -15,6 +15,9 @@ public class Ventana_Mapa implements IVentana {
 
     private int WIDTH;
     private int HEIGHT;
+    private int casillaHeight;
+    private int casillaWidth;
+    Mapa map;
 
     public Ventana_Mapa(int WIDTH, int HEIGHT) {
         this.WIDTH = WIDTH;
@@ -24,12 +27,36 @@ public class Ventana_Mapa implements IVentana {
     public void draw(Graphics2D g) {
         g.setColor(new Color(0xEBC053));
         g.fillRect(0, 0, WIDTH, HEIGHT);
+        for(int i =0; i < map.getMapa().length;i++ ){
+            for (int j=0;j<map.getMapa().length;j++){
+                g.setColor(Color.green);
+                if(map.getMapa()[i][j]>0)
+                    g.fillRect(j*casillaWidth,i*casillaHeight,  casillaWidth, casillaHeight);
+            }
+        }
+
     }
 
     public void update() {
     }
 
     public void cargar() {
+        int[][] mapa=
+        {
+            {0,1,1,1,1,1,1,1,1,1},
+            {0,1,1,0,0,0,1,1,1,1},
+            {0,0,1,0,1,0,1,1,1,1},
+            {1,0,0,0,1,0,1,1,1,1},
+            {1,1,1,1,1,0,1,0,0,0},
+            {1,1,1,1,1,0,1,0,1,1},
+            {1,1,1,1,1,0,1,0,1,1},
+            {1,1,1,1,1,0,0,0,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1}
+        };
+        casillaHeight = HEIGHT / mapa[0].length;
+        casillaWidth = WIDTH / mapa[0].length;
 
+        map = new Mapa(mapa);
     }
 }
