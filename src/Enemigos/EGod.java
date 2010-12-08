df1eed3200d1eaf8c88bc8e937bc34d244b7610c
Thanar <Thankor@gmail.com>
2010-12-08 18:07:09 +0100

    Signed-off-by: Thanar <Thankor@gmail.com>

---
+++ src/Enemigos/EGod.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Enemigos;
+
+import Personajes.Enemy;
+import UtilMath.Vector2D;
+
+/**
+ *
+ * @author alumno
+ */
+public class EGod extends Enemy {
+
+    public EGod(int nivel, Vector2D posicion){
+        super(9, (float)(10+(1*nivel)), 400 + 400*nivel, 50+nivel,7,6,posicion, "imagenes/mounstrillo.png",50);
+    }
+
+}
